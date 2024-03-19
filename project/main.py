e084279b12aac8d3049c7f1724d1f417fe07c232
Scrappyz <ghostmic3000@gmail.com>
2024-03-19 19:19:10 +0800

    added rephraseComment

--- project/main.py
+++ project/main.py
@@ -1,5 +1,44 @@
+import json
+from pathlib import Path
+
+def getRephrasals(path: str) -> dict:
+    f = open(path)
+    data = json.load(f)
+    rephrasals = {}
+    for k, v in data.items():
+        key = k.lower()
+        for i in v:
+            rephrasals[i.lower()] = key
+    return rephrasals
+
+def rephraseComment(comment: str, rephrasals: dict) -> str:
+    include = {"\'"}
+    rephrased_comment = ""
+    word = ""
+    for i in range(len(comment)):
+        ch = comment[i]
+        if not ch.isalpha() and ch not in include:
+            if len(word) > 0:
+                if word in rephrasals:
+                    word = rephrasals[word]
+                rephrased_comment += word
+                word = ""
+            rephrased_comment += ch
+            continue
+        word += ch.lower()
+        
+    if len(word) > 0:
+        if word in rephrasals:
+            word = rephrasals[word]
+        rephrased_comment += word
+        word = ""
+        
+    return rephrased_comment
+
 def main():
-    print("Hello World!!")
+    current_dir = Path(__file__).parent.resolve()
+    rephrasals = getRephrasals(Path.joinpath(current_dir, "rephrase.json"))
+    print(rephraseComment("Fuck you, you shouldn't do that", rephrasals))
 
 if __name__ == "__main__":
     main()
--- test/test_main.py
+++ test/test_main.py
@@ -4,9 +4,13 @@ sys.path.append(str(pathlib.Path(__file__).parents[1].joinpath("project").resolv
 
 import main
 
+current_dir = pathlib.Path(__file__).parent.resolve()
+rephrasals = main.getRephrasals(pathlib.Path.joinpath(current_dir, "../project/rephrase.json"))
+
 class TestMain(unittest.TestCase):
     def test_main(self):
-        self.assertEqual(5, 5)
+        self.assertEqual(main.rephraseComment("I won't do that", rephrasals), "i will not do that")
+        self.assertEqual(main.rephraseComment("I shouldn't do that", rephrasals), "i should not do that")
         
 if __name__ == "__main__":
     unittest.main()
