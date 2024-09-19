831379e519cb4e246838ae116c1a290f62052e06
Scrappyz <ghostmic3000@gmail.com>
2024-09-19 19:14:00 +0800

    Added translator

--- project/main.py
+++ project/main.py
@@ -1,48 +1,12 @@
-import json
-from pathlib import Path
-
-# Gets contractions.json content into a dict.
-# Parameters:
-# `path`: Path to the contractions JSON file.
-def getContractions(path: str) -> dict:
-    f = open(path)
-    data = json.load(f)
-    contractions = {}
-    for k, v in data.items():
-        key = k.lower()
-        for i in v:
-            contractions[i.lower()] = key
-    return contractions
-
-# Expands contractions like "don't" -> "do not".    
-def expandContractions(comment: str, contractions: dict) -> str:
-    include = {"\'"}
-    cleaned_comment = ""
-    word = ""
-    for i in range(len(comment)):
-        ch = comment[i]
-        if not ch.isalpha() and ch not in include:
-            if len(word) > 0:
-                if word in contractions:
-                    word = contractions[word]
-                cleaned_comment += word
-                word = ""
-            cleaned_comment += ch
-            continue
-        word += ch.lower()
-        
-    if len(word) > 0:
-        if word in contractions:
-            word = contractions[word]
-        cleaned_comment += word
-        word = ""
-        
-    return cleaned_comment
+import argparse
+from deep_translator import GoogleTranslator
 
 def main():
-    current_dir = Path(__file__).parent.resolve()
-    contractions = getContractions(Path.joinpath(current_dir, "resources/contractions.json"))
-    print(expandContractions("Fuck you, you shouldn't do that", contractions))
+    text = "Magandang hapon!"
+    translator = GoogleTranslator(source="auto")
+    translator.target = "english"
+    
+    print(translator.translate(text))
 
 if __name__ == "__main__":
     main()
