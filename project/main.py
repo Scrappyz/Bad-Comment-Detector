ca9019994d1dda9156760f1bf6070b7b2b049159
Scrappyz <ghostmic3000@gmail.com>
2024-09-19 19:26:22 +0800

    Added argparser

--- project/main.py
+++ project/main.py
@@ -2,9 +2,16 @@ import argparse
 from deep_translator import GoogleTranslator
 
 def main():
-    text = "Magandang hapon!"
-    translator = GoogleTranslator(source="auto")
-    translator.target = "english"
+    parser = argparse.ArgumentParser(description='Detect bad comment.')
+    parser.add_argument('sentence', type=str, nargs=1,
+                         help='bad comment.')
+    
+    args = parser.parse_args()
+    
+    text = args.sentence[0]
+    translator = GoogleTranslator()
+    translator.source = 'filipino'
+    translator.target = 'english'
     
     print(translator.translate(text))
 
