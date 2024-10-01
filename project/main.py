2f6113170b2e32018f029802afd697ec7435a49e
Scrappyz <ghostmic3000@gmail.com>
2024-10-01 22:27:46 +0800

    Revamped data cleaning

--- project/main.py
+++ project/main.py
@@ -4,6 +4,7 @@ import os
 import re
 import json
 import contractions
+import ahocorasick
 from thefuzz import fuzz
 from pathlib import Path
 
@@ -15,7 +16,11 @@ nlp = spacy.load("en_core_web_md")
 # Mapping for leetspeak to regular characters
 leet_map = {
     '0': 'o', '1': 'i', '3': 'e', '4': 'a', '5': 's', '7': 't',
-    '@': 'a', '$': 's', '!': 'i', '*': '', '#': '', '(': 'c'
+    '@': 'a', '$': 's', '(': 'c'
+}
+
+wilcards = {
+    '*', '#'
 }
 
 def readTextFromFile(file_path: str) -> str:
@@ -26,127 +31,90 @@ def readTextFromFile(file_path: str) -> str:
 def readJsonFromFile(file_path: str):
     with open(file_path, "r") as f:
         return json.load(f)
+    
+def findMatchingSubstrings(text, word_list, wildcards: str) -> dict:
+    substrings = {}
+    wildcards_set = set()
+    
+    # Add wildcards to set to be able to compare multiple wildcards
+    for i in wildcards:
+        wildcards_set.add(i)
 
-# Turns non-alphabets into any one character regex pattern
-def toRegex(text: str) -> str:
-    s = ""
-    for i in text.lower():
-        if ord(i) >= ord('a') and ord(i) <= ord('z') or i == ' ':
-            s += i
-            continue
-        
-        if i == '.':
-            s += '\\'
-        
-        s += '.'
-        
-    return s
-
-# replaces words with bad words
-def fuzzyReplace(words, keywords, threshold=65):
-    for i in range(len(words)):
-        if words[i].find('*') < 0:
-            continue
-        
-        for word in keywords:
-            if fuzz.ratio(words[i], word) >= threshold:
-                # print(words[i] + ", " + word)
-                words[i] = word
-                break
-            
-def findMatchingSubstrings(s, l, wildcard: str) -> set:
-    substrings = set()
-
-    for word in l:
+    # Loop through the word list
+    for word in word_list:
         i = 0
         j = 0
-        while i < len(s) and j < len(word):
-            if s[i] == word[j] or s[i] == wildcard:
+        substr = "" # Temporary variable to store the found substring
+        
+        # Loop until whole text is traversed
+        while i < len(text):
+            
+            # If both `i` and `j` point to the same character, it means a possible substring is found.
+            # Increment both `i` and `j` so they will traverse with each other.
+            if text[i] == word[j] or text[i] in wildcards_set:
+                substr += text[i]
                 i += 1
                 j += 1
-            else:
+            else: # Otherwise, reset `j` and `substr`
                 i = i - j + 1
                 j = 0
-        if j == len(word):
-            substrings.add(word)
+                substr = ""
+                
+            # If `j` reaches the end of `word`, it means a substring was found.
+            if j == len(word):
+                substrings[substr] = word
+                substr = ""
+                i += 1
+                j = 0
 
-    return substrings
-
-def replaceNonAlphabetWithWildcard(words):
-    str = ""
-    for i in range(len(words)):
-        str = ""
-        for j in range(len(words[i])):
-            if ord(words[i][j]) >= ord('a') and ord(words[i][j]) <= ord('z'):
-                str += words[i][j]
-            else:
-                str += '*'
-        words[i] = str
-    return words
-
-def getOriginalComment(text, keywords: set, threshold=65):
-    # Expand contractions
-    expanded_text = contractions.fix(text.lower())
+    return dict(reversed(list(substrings.items())))
     
-    # Tokenize the text
-    tokens = nlp(expanded_text, disable=["parser", "tagger", "ner", "lemmatizer", "textcats"])
-    words = []
+def normalizeWildcards(text: str) -> str:
+    normalized = ""
+    for i in text:
+        if i in wilcards:
+            normalized += '*'
+            continue
+            
+        normalized += i
+        
+    return normalized
 
-    for token in tokens:
-        words.append(token.text)
-
-    return words
-
-# returns bad words
-def cleanTextAndTokenize(text, keywords: set, threshold=65) -> list:
-    # Expand contractions
-    expanded_text = contractions.fix(text.lower())
+def cleanText(text: str, word_set) -> str:
+    text = contractions.fix(text)
     
-    # Tokenize the text
-    tokens = nlp(expanded_text, disable=["parser", "tagger", "ner", "lemmatizer", "textcats"])
-    words = []
+    print("1.) ", text)
+    cleaned = ""
+    for i in text.lower():
+        if i in leet_map:
+            cleaned += leet_map[i]
+            continue
+        
+        cleaned += i
     
-    # Remove punctuation tokens
-    for token in tokens:
-        # print("Token: ", token)
-        # Check if the token is alphanumeric (contains letters or digits)
-        # if not re.fullmatch(r'^[\W_]+$', token.text):  # Matches tokens with only special characters
-        #     words.append(token.text)  # Append the actual text representation of the token
-        words.append(token.text)
-
-    replaceNonAlphabetWithWildcard(words)
-    for word in words:
-        print("Current: " + word)
+    print("2.) ", cleaned)
+    cleaned = normalizeWildcards(cleaned)
+    print("3.) ", cleaned)
     
-    # fuzzyReplace(words, keywords, threshold)
-
-    badWordsList = list(keywords)
-
-    for i in range(len(words)):
-        # if words[i].find('*') < 0:
-        #     continue
-        words[i] = parser.fuzzyMatchWord(words[i], badWordsList)
-
-    for word in words:
-        print("Fuzzy: " + word)
-
-    for i in range(len(words)):
-        words[i] = parser.parseBadWord(words[i], badWordsList)
+    matches = findMatchingSubstrings(cleaned, word_set, "*")
     
-    # for i in range(len(words)):
-    #     for badword in keywords:
-    #         if words[i].find(badword) > -1:
-    #             words[i] = badword
-
-    for word in words:
-        print("Regex: " + word)
+    for k, v in matches.items():
+        cleaned = cleaned.replace(k, v)
     
-    return words
+    print("4.) ", cleaned)
+    
+    return cleaned
 
 def ruleBasedDetection(tokens, keywords: set):
     # Detect toxicity using rule-based regex patterns.
+    # automaton = ahocorasick.Automaton(ahocorasick.STORE_INTS)
+    # for i in keywords:
+    #     automaton.add_word(i)
+        
+    # automaton.make_automaton()
+    
     for i in tokens:
-        if i in keywords:
+        if str(i) in keywords:
             return True
     return False
 
@@ -163,8 +131,7 @@ def aiBasedDetection(text, nlp):
 def detectToxicity(text, keywords: set, nlp, threshold=65):
     # Hybrid approach combining rule-based and AI-based toxicity detection.
     # Step 1: Clean the text
-    tokens = cleanTextAndTokenize(text, keywords, threshold)
-    originalTokens = getOriginalComment(text, keywords, threshold)
+    tokens = nlp(cleanText(text, keywords))
     
     # Step 2: Rule-based detection
     if ruleBasedDetection(tokens, keywords):
@@ -182,13 +149,9 @@ def main():
     toxic_keywords = set(readJsonFromFile(asset_dir.joinpath("toxic_keywords.json")))
     essay = readTextFromFile(source_dir.parent.joinpath("data/essay.txt").resolve())
     
-    for i in test_cases:
-        comment = i["comment"]
-        expected_result = i["expected"]
-        is_toxic = detectToxicity(comment, toxic_keywords, nlp)
-        actual_result = "Toxic" if is_toxic else "Non-Toxic"
-        
-        print("[{0}] ({1}): {2}".format(actual_result, expected_result, comment))
+    text = "You f#ck1ng fuc a$$h0l5"
+    cleaned = cleanText(text, toxic_keywords)
+    print(cleaned)
 
 def main_test():
     """Main function to run toxicity detection on test cases."""
@@ -196,7 +159,7 @@ def main_test():
     asset_dir = source_dir.parent.joinpath("assets")
     test_cases = list(readJsonFromFile(asset_dir.joinpath("test_cases.json")))
     toxic_keywords = set(readJsonFromFile(asset_dir.joinpath("toxic_keywords.json")))
-    # essay = readTextFromFile(source_dir.parent.joinpath("data/essay.txt").resolve())
+    
     score = 0
     total = 0
     pass_test_case = False
