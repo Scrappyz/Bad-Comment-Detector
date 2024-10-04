ad9d2e7a9e1ea06f92546d8e8fc481c6bed4fbab
pvzzombs <pvzzombs@gmail.com>
2024-10-04 21:18:04 +0800

    Modify model.py to load a larger data set,
    Add some print statements to notify user

--- project/model.py
+++ project/model.py
@@ -2,32 +2,35 @@ import spacy
 from spacy.tokens import DocBin
 
 import helper
+# import main
 
-def loadAndPrepareDataSet(nlp):
-  test_cases = list(helper.readJsonFromFile("../assets/test_cases.json"))
-  l = []
-  for t in test_cases:
-    doc1 = nlp(t["comment"])
-    doc2 = nlp(t["cleaned"])
-    if t["expected"] == "Toxic":
-      doc1.cats["toxic"] = 1
-      doc1.cats["non-toxic"] = 0
-      doc2.cats["toxic"] = 1
-      doc2.cats["non-toxic"] = 0
-    else:
-      doc1.cats["toxic"] = 0
-      doc1.cats["non-toxic"] = 1
-      doc2.cats["toxic"] = 0
-      doc2.cats["non-toxic"] = 1
-    l.append(doc1)
-    l.append(doc2)
-  return l
+# def loadAndPrepareDataSet(nlp):
+#   test_cases = list(helper.readJsonFromFile("../assets/test_cases.json"))
+#   l = []
+#   for t in test_cases:
+#     doc1 = nlp(t["comment"])
+#     doc2 = nlp(t["cleaned"])
+#     if t["expected"] == "Toxic":
+#       doc1.cats["toxic"] = 1
+#       doc1.cats["non-toxic"] = 0
+#       doc2.cats["toxic"] = 1
+#       doc2.cats["non-toxic"] = 0
+#     else:
+#       doc1.cats["toxic"] = 0
+#       doc1.cats["non-toxic"] = 1
+#       doc2.cats["toxic"] = 0
+#       doc2.cats["non-toxic"] = 1
+#     l.append(doc1)
+#     l.append(doc2)
+#   return l
+
 def loadAndPrepareDataSetFromCSV(nlp, startRange, endRange):
   t = helper.readCSVFromFile("../assets/labeled_data.csv")
+  # print("Length: " + str(len(t)))
   l = []
   if endRange == None:
     endRange = len(t)
-  print(endRange)
+  # print(endRange)
   # print(t[1][5])
   # print(t[1][6])
   for i in range(startRange, endRange):
@@ -46,10 +49,11 @@ def loadAndPrepareDataSetFromCSV(nlp, startRange, endRange):
 
 def loadYoutubeComments(nlp, startRange, endRange):
   t = helper.readCSVFromFile("../assets/youtoxic_english_1000.csv")
+  # print(len(t))
   l = []
   if endRange == None:
     endRange = len(t)
-  print(endRange)
+  # print(endRange)
   # print(t[1][5])
   # print(t[1][6])
   for i in range(startRange, endRange):
@@ -71,25 +75,58 @@ def loadYoutubeComments(nlp, startRange, endRange):
     l.append(doc1)
   return l
 
+def loadTCCC(nlp, startRange, endRange):
+  t = helper.readCSVFromFile("../assets/train.csv")
+  # print("Length is " + str(len(t)))
+  # print("Comment: " + t[1][1])
+  l = []
+  if endRange == None:
+    endRange = len(t)
+  print(endRange)
+  for i in range(startRange, endRange):
+    # print(t[6])
+    doc1 = nlp(t[i][1])
+    isToxic = False
+    for j in range(2, 8):
+      if t[i][j] == "1":
+        isToxic = True
+        break
+    if isToxic:
+      # print("Hello")
+      doc1.cats["toxic"] = 1
+      doc1.cats["non-toxic"] = 0
+    else:
+      # print("Not")
+      doc1.cats["toxic"] = 0
+      doc1.cats["non-toxic"] = 1
+    l.append(doc1)
+  return l
+
 def loadAllDPossibleTrainDataSets(nlp):
   l = []
-  # l += loadAndPrepareDataSetFromCSV(nlp, 1, 13477)
-  l += loadYoutubeComments(nlp, 1, 500)
+  l += loadAndPrepareDataSetFromCSV(nlp, 1, 18588)
+  l += loadYoutubeComments(nlp, 1, 750)
+  # l += loadTCCC(nlp, 1, 119679)
   return l
 
 def loadAllPossibleValidationDataSets(nlp):
   l = []
-  # l += loadAndPrepareDataSetFromCSV(nlp, 13477, None)
-  l += loadYoutubeComments(nlp, 500, None)
+  l += loadAndPrepareDataSetFromCSV(nlp, 18588, None)
+  l += loadYoutubeComments(nlp, 750, None)
+  # l += loadTCCC(nlp, 119679, None)
   return l
 
 if __name__ == "__main__":
+  print("----- model.py -----")
+  print("Preparing data sets...")
   nlp = spacy.load("en_core_web_md")
   trainData = loadAllDPossibleTrainDataSets(nlp)
   validationData = loadAllPossibleValidationDataSets(nlp)
-  print(len(trainData))
-  print(len(validationData))
+  # print(len(trainData))
+  # print(len(validationData))
+  print("------ Saving data ------")
   binaryTrainData = DocBin(docs=trainData)
   binaryTrainData.to_disk("../data/train.spacy")
   binaryValidData = DocBin(docs=validationData)
   binaryValidData.to_disk("../data/valid.spacy")
+  print("------ DONE ------")
