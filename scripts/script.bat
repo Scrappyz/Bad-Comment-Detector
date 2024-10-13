38dfbfb24f58d216dd88da1115e42bf003233a63
Scrappyz <ghostmic3000@gmail.com>
2024-10-13 15:26:23 +0800

    Added new script

--- scripts/script.bat
+++
@@ -1,3 +0,0 @@
-python ../detector/model.py
-python -m spacy init fill-config ../config/base_config.cfg ../config/config.cfg
-python -m spacy train ../config/config.cfg --output ../output
---
+++ scripts/train.py
@@ -0,0 +1,10 @@
+import subprocess
+from pathlib import Path
+
+if __name__ == "__main__":
+    source_dir = Path(__file__).parent.resolve()
+    root_dir = source_dir.parent.resolve()
+    subprocess.run(["python", root_dir.joinpath("detector/model.py").resolve()])
+    subprocess.run(["python", "-m", "spacy", "init", "fill-config", root_dir.joinpath("config/base_config.cfg").resolve(), root_dir.joinpath("config/config.cfg").resolve()])
+    subprocess.run(["python", "-m", "spacy", "train", root_dir.joinpath("config/config.cfg").resolve(), "--output", root_dir.joinpath("output").resolve()])
+
