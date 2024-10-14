0e399051da10d921b2d7b2cdedea279889062ce9
Scrappyz <ghostmic3000@gmail.com>
2024-10-14 11:29:44 +0800

    update

--- scripts/train.py
+++ scripts/train.py
@@ -6,5 +6,5 @@ if __name__ == "__main__":
     root_dir = source_dir.parent.resolve()
     subprocess.run(["python", root_dir.joinpath("detector/model.py").resolve()])
     subprocess.run(["python", "-m", "spacy", "init", "fill-config", root_dir.joinpath("config/base_config.cfg").resolve(), root_dir.joinpath("config/config.cfg").resolve()])
-    subprocess.run(["python", "-m", "spacy", "train", root_dir.joinpath("config/config.cfg").resolve(), "--output", root_dir.joinpath("output").resolve()])
+    subprocess.run(["python", "-m", "spacy", "train", root_dir.joinpath("config/config.cfg").resolve(), "--output", root_dir.joinpath("output").resolve(), "--gpu-id", "0"])
 
