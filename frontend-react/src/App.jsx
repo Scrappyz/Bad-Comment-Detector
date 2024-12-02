33bdf12c5572425ee38460a45f6ed289777d7f4e
pvzzombs <pvzzombs@gmail.com>
2024-12-02 13:40:31 +0800

    Support Enter key on press

--- frontend-react/src/App.jsx
+++ frontend-react/src/App.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios'
 function App() {
-  function tryGetCategory(event) {
+  function getOutputUsingPostRequest() {
     axios.post('http://127.0.0.1:8000/api', {
       'text': document.getElementById('txt1').value
     }).then(function (response){
@@ -8,10 +8,18 @@ function App() {
       document.getElementById("result").innerText = response.data["result"];
     });
   }
+  function tryGetCategory() {
+    getOutputUsingPostRequest();
+  }
+  function tryGetCategoryKeyDown(event) {
+    if (event.key === "Enter") {
+      getOutputUsingPostRequest();
+    }
+  }
   return (
     <div>
       <div>
-        <input type='text' id='txt1' />
+        <input type='text' id='txt1' onKeyDown={tryGetCategoryKeyDown}/>
         <input type="button" id='btn1' value='Test' onClick={tryGetCategory} />
         <div id='result'> Result will appear here </div>
       </div>
