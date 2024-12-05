be5b1ee9f88842582b9afe0c7e2cddd50214aadc
Scrappyz <ghostmic3000@gmail.com>
2024-12-05 21:42:11 +0800

    Added dropdown button

--- frontend-react/src/App.jsx
+++ frontend-react/src/App.jsx
@@ -1,63 +1,56 @@
-import React from 'react';
-import {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import "./App.css";
 import Result from "./components/Result.jsx";
 
 function App() {
+  const [connection, setConnection] = useState(false);
+  const [result, setResult] = useState("Empty");
+  const [resultColor, setResultColor] = useState();
+  const [dropdown, setDropdown] = useState(false);
+  const [inputValue, setInputValue] = useState('');
+  const [expectedResult, setExpectedResult] = useState("Result");
+
   function heartBeat() {
-    axios.get("https://bad-comment-detector-server.onrender.com/").then(function() {
-      // alert("Connected!");
-      setConnection(true);
-    }).catch(function() {
-      setConnection(false);
-    });
+    axios.get("https://bad-comment-detector-server.onrender.com/")
+      .then(() => setConnection(true))
+      .catch(() => setConnection(false));
   }
 
-  useEffect(function(){
-    setInterval(heartBeat, 5000);
-    if (localStorage.getItem("limit") === null) {
+  useEffect(() => {
+    const interval = setInterval(heartBeat, 5000);
+    if (!localStorage.getItem("limit")) {
       localStorage.setItem("limit", "100");
-      let nextTime = (new Date()).getTime();
-      nextTime = nextTime + 60 * 60 * 1000;
+      const nextTime = Date.now() + 60 * 60 * 1000;
       localStorage.setItem("nextTime", nextTime.toString());
     }
+    return () => clearInterval(interval);
   }, []);
 
   function getOutputUsingPostRequest() {
-    var limit = parseInt(localStorage.getItem("limit"));
-    var nextTime = parseInt(localStorage.getItem("nextTime"));
-    var currentTime = (new Date()).getTime();
+    const limit = parseInt(localStorage.getItem("limit"));
+    const nextTime = parseInt(localStorage.getItem("nextTime"));
+    const currentTime = Date.now();
+
     if (limit < 1 && currentTime < nextTime) {
       alert("Limit exceeded");
-      return ;
+      return;
     }
+
     if (currentTime >= nextTime) {
-      limit = 5;
       localStorage.setItem("limit", "100");
-      nextTime = (new Date()).getTime();
-      nextTime = nextTime + 60 * 60 * 1000;
-      localStorage.setItem("nextTime", nextTime.toString());
+      const newNextTime = Date.now() + 60 * 60 * 1000;
+      localStorage.setItem("nextTime", newNextTime.toString());
     }
 
-    axios.post('https://bad-comment-detector-server.onrender.com/api', {
-      'text': document.getElementById('txt1').value
-    }).then(function (response){
-      // console.log(response);
-      // document.getElementById("result").innerText = response.data["result"];
-      let result = response.data["result"];
-      if(result === "toxic") {
-        setResult("Toxic");
-        setResultColor("red");
-      } else {
-        setResult("Clean");
-        setResultColor("green");
-      }
-    });
+    axios.post('https://bad-comment-detector-server.onrender.com/api', { text: inputValue })
+      .then((response) => {
+        const result = response.data.result;
+        setResult(result === "toxic" ? "Toxic" : "Clean");
+        setResultColor(result === "toxic" ? "red" : "green");
+      });
 
-    limit = limit - 1;
-    localStorage.setItem("limit", limit.toString());
-
+    localStorage.setItem("limit", (limit - 1).toString());
   }
 
   function tryGetCategory() {
@@ -70,38 +63,70 @@ function App() {
     }
   }
 
-  const [connection, setConnection] = useState(false);
-  const [result, setResult] = useState("Empty");
-  const [resultColor, setResultColor] = useState();
+  const toggleDropdown = () => setDropdown((prevState) => !prevState);
+
+  useEffect(() => {
+    const handleOutsideClick = (event) => {
+      if (!event.target.closest('.dropdown')) {
+        setDropdown(false);
+      }
+    };
+    document.addEventListener('click', handleOutsideClick);
+
+    return () => {
+      document.removeEventListener('click', handleOutsideClick);
+    };
+  }, []);
+
+  const selectExpectedResult = (result) => {
+    setExpectedResult(result);
+    setDropdown(false);
+  }
 
   return (
-    <div className='frame'>
-      <div className='form'>
-        <div className='header'>
-          Bad Comment Detector
-        </div>
-        <div className='content'>
-          <div className='status-group' >
-            <div className='label-box'>
-              Status
-            </div>
-            {connection ? <img className='status' src='../public/check.svg' style={{backgroundColor: "rgb(0, 234, 0)"}} /> : <img className='status' src='../public/cross.svg' style={{backgroundColor: "red"}} />}
+    <div className="frame">
+      <div className="form">
+        <div className="header">Bad Comment Detector</div>
+        <div className="content">
+          <div className="status-group">
+            <div className="label-box">Status</div>
+            {connection ? (
+              <img className="status" src="../public/check.svg" style={{ backgroundColor: "rgb(0, 234, 0)" }} />
+            ) : (
+              <img className="status" src="../public/cross.svg" style={{ backgroundColor: "red" }} />
+            )}
           </div>
-          <div className='input-group'>
-            <input className='input' placeholder='Enter your comment here' type='text' id='txt1' onKeyDown={tryGetCategoryKeyDown}/>
-            <input className='btn' type="button" id='btn1' value='Test' onClick={tryGetCategory} />
+          <div className="input-group">
+            <input
+              className="input"
+              placeholder="Enter your comment here"
+              type="text"
+              value={inputValue}
+              onChange={(e) => setInputValue(e.target.value)}
+              onKeyDown={tryGetCategoryKeyDown}
+            />
+            <input className="btn" type="button" id="btn1" value="Test" onClick={tryGetCategory} />
           </div>
           <Result text={result} textColor={resultColor} />
-          <div className='feedback-group'>
-            <div className='label-box'>
-              Expected
+          <div className="feedback-group">
+            <div className="expected-group">
+              <div className="label-box">Expected</div>
+              <div className="dropdown">
+                <button onClick={toggleDropdown} className="btn">{expectedResult}</button>
+                {dropdown && (
+                  <div className="dropdown-items">
+                    <button onClick={() => selectExpectedResult("Clean")} className="items">Clean</button>
+                    <button onClick={() => selectExpectedResult("Toxic")} className="items">Toxic</button>
+                  </div>
+                )}
+              </div>
             </div>
-            <button className='btn'>Result</button>
+            <button className="send-btn">Send</button>
           </div>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
